77aa66778c16a0d3dae9046ea91c66906dd687ca
peter_heard01 <peter_heard01@hotmail.com>
2015-03-08 14:30:02 +0000

    added in setup tests

--- TicTacToe.Test/TicTacToeEngineTests.cs
+++ TicTacToe.Test/TicTacToeEngineTests.cs
@@ -1,19 +1,44 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TicTacToe.Test
 {
-    [TestClass]
+    [TestFixture]
     public class TicTacToeEngineTests
     {
-        [TestMethod]
+        [Test]
         public void On_Game_Start_Game_Shows_Empty_Board_And_Prompts_User_To_Choose_Symbol()
         {
-            var gameState = new Game().Generate(null);
-            
-            Assert.AreEqual(gameState.Message, "Please choose your symbol");
-            CollectionAssert.AreEqual(gameState.Board, new string[9] { "_", "_", "_", "_", "_", "_", "_", "_", "_" });
+            var game = new Game();
+            game.Generate();
+
+            Assert.AreEqual(game.GameState.Message, "Please choose your symbol");
+            CollectionAssert.AreEqual(game.GameState.Board, new string[9] { "_", "_", "_", "_", "_", "_", "_", "_", "_" });
+        }
+
+        [Test]
+        public void User_Chooses_To_Be_x_Then_Game_Asks_If_They_Want_To_Go_First_And_Computer_Is_o()
+        {
+            var game = new Game();
+            game.UserSymbol = "x";
+            game.Generate();
+
+            Assert.AreEqual(game.ComputerSymbol, "o");
+            Assert.AreEqual(game.GameState.Message, "Would you like to go first?");
+            CollectionAssert.AreEqual(game.GameState.Board, new string[9] { "_", "_", "_", "_", "_", "_", "_", "_", "_" });
+        }
+
+        [Test]
+        public void User_Chooses_To_Be_o_Then_Game_Asks_If_They_Want_To_Go_First_And_Computer_Is_x()
+        {
+            var game = new Game();
+            game.UserSymbol = "o";
+            game.Generate();
+
+            Assert.AreEqual(game.ComputerSymbol, "x");
+            Assert.AreEqual(game.GameState.Message, "Would you like to go first?");
+            CollectionAssert.AreEqual(game.GameState.Board, new string[9] { "_", "_", "_", "_", "_", "_", "_", "_", "_" });
         }
 
         
--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -8,12 +8,35 @@ namespace TicTacToe
 {
     public class Game
     {
-        public GameState Generate(int? position)
+        public string UserSymbol { get; set; }
+
+        public string ComputerSymbol { get; set; }
+
+        public GameState GameState { get; set; }
+
+        public Game()
         {
-            var state = new GameState();
-            state.Message = "Please choose your symbol";
-            state.Board = new string[9] { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
-            return state;
+            GameState = new GameState();
+        }
+
+        public void Generate()
+        {
+            if (UserSymbol == null)
+            {
+                GameState.Message = "Please choose your symbol";
+            }
+            else
+            {
+                GameState.Message = "Would you like to go first?";
+                ComputerSymbol = Flip(UserSymbol);
+            }
+
+            GameState.Board = new string[9] { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
+        }
+
+        private string Flip(string symbolArg)
+        {
+            return symbolArg == "x" ? "o" : "x";
         }
     }
 }
